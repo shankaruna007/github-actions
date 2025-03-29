028f3fbdfb4596b343ff3cb04bf91f2e7e1432c4
shankaruna007 <shankarunakara3@gmail.com>
2025-03-29 09:49:45 +0000

    Update variables.tf

--- tf-example/variables.tf
+++ tf-example/variables.tf
@@ -1,3 +1,16 @@
 variable "ec2_name" {
-  type = string
+  type        = string
+  description = "Name of the EC2 instance"
+  default     = Dev server
+}
+
+variable "instance_type" {
+  type        = string
+  description = "EC2 instance type"
+  default     = "t2.micro"
+}
+
+variable "key_name" {
+  type        = string
+  description = "SSH Key Name"
 }
