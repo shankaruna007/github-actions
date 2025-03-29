db3d5b1ce70829bb5d4dffb3cd07455b00272234
shankaruna007 <shankarunakara3@gmail.com>
2025-03-29 09:48:37 +0000

    Update main.tf

--- tf-example/main.tf
+++ tf-example/main.tf
@@ -1,27 +1,23 @@
 data "aws_ami" "ubuntu" {
-    most_recent = true
+  most_recent = true
 
-    filter {
-        name   = "name"
-        values = ["ubuntu/images/hvm-ssd/*20.04-amd64-server-*"]
-    }
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/*20.04-amd64-server-*"]
+  }
 
-    filter {
-        name   = "virtualization-type"
-        values = ["hvm"]
-    }
-    
-    owners = ["099720109477"] # Canonical
-}
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
 
-provider "aws" {
-  region  = "us-east-2"
+  owners = ["099720109477"] # Canonical
 }
 
 resource "aws_instance" "app_server" {
   ami           = data.aws_ami.ubuntu.id
-  instance_type = "t3.micro"
-  key_name      = "app-ssh-key"
+  instance_type = var.instance_type
+  key_name      = var.key_name
 
   tags = {
     Name = var.ec2_name
